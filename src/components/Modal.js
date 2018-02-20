99fd559ff6ac5c8cfd00991ae172a99bff9c91cd
martina weidenbaum <martina.weidenbaum@gmail.com>
2018-02-20 13:06:45 -0500

    new API key

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -22,7 +22,7 @@ export default class MapModal extends Component {
   	const MyMapComponent = compose(
 	  withProps({
 	    googleMapURL:
-	      "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
+	      "https://maps.googleapis.com/maps/api/js?key=AIzaSyAaxDUM4amBAQ_ppmk5AE4X5nMg4VAwTQg&v=3.exp&libraries=geometry,drawing,places",
 	    loadingElement: <div style={{ height: `100%` }} />,
 	    containerElement: <div style={{ height: `400px` }} />,
 	    mapElement: <div style={{ height: `100%` }} />
