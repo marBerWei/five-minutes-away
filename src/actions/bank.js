2cc3fb04f70ff39e6ce143c988cf6dd1ec64107f
martina weidenbaum <martina.weidenbaum@gmail.com>
2018-02-23 13:43:27 -0500

    deleted comments

--- src/actions/bank.js
+++ src/actions/bank.js
@@ -1,15 +1,11 @@
 export function fetchBanks(ll, walkOrDrive) {
   let dist = walkOrDrive === "walking" ? 322 : 2500
   return function(dispatch) {
-  	//convert address to string
+
     dispatch(fetchingBanks())
     fetch(`https://api.foursquare.com/v2/venues/search?ll=${ll}&client_id=EFQTHVDSAO0XRYYHCW3ZUUR0Z3HRRWZ4YUXJ1G3VKZYATKY1&client_secret=ZFEIUSVWO005JXIGMTXD0DQRKLXLVDPX4YOPQTMC41MYTB0Y&v=20180218&query=bank&radius=${dist}&limit=5`, {
         method: 'get'
-       //  headers : { 
-       //  'Content-Type': 'application/json',
-       //  'Accept': 'application/json'
-       // }
-        })
+      })
       .then((res) => res.json())
       .then((json) => {
         console.log(json.response.venues)
